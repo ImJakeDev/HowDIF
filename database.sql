a544c6eec2d0339450469b961b9a40facd49f9be
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-07 14:00:08 -0500

    updated database.sql with tables

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+-- Create a database named 'howdif'.
 
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
@@ -6,5 +7,19 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (225) NOT NULL,
+    "firstname" VARCHAR (225) NOT NULL,
+    "lastname" VARCHAR (225) NOT NULL
+);
+-- Table holds all the data for each log created on the app
+CREATE TABLE "emotion_logged" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"date" timestamp NOT NULL DEFAULT NOW(),
+	"primary_emotion" varchar(255) NOT NULL,
+	"intensity_emotion" varchar(255) NOT NULL,
+	"intensity_level" integer NOT NULL,
+	"how_feel" varchar(255),
+	"why_feel" varchar(255)
 );

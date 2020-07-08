70de9be1aadeaf871d63bd2befdcdf6308c65099
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-08 14:54:40 -0500

    setting up the emotion log process with all sorts of fun things!

--- src/components/Utilities/LogEmotionsButton/LogEmotionsButton.js
+++ src/components/Utilities/LogEmotionsButton/LogEmotionsButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import {
   Button,
@@ -11,7 +11,32 @@ import {
 } from "@material-ui/core";
 
 const LogEmotionsButton = (props) => {
-  const [open, setOpen] = React.useState(false);
+  // ----- Functional State with React useState Hook -----
+  // primary_emotion - State default null
+  const [primaryEmotion, setPrimaryEmotion] = useState(null);
+  // intensity_emotion - State default null
+  const [intensityEmotion, setIntensityEmotion] = useState(null);
+  // intensity_level - State default 0
+  const [intensityLevel, setIntensityLevel] = useState(0);
+  // how_feel - State default ""
+  const [howFeel, setHowFeel] = useState("");
+  // why_feel - State default ""
+  const [whyFeel, setWhyFeel] = useState("");
+  // Stage state which will render the correct stage of the process of the emotion log
+  // default is Stage 1 - Will begin at Stage 1
+  const [stage, setStage] = useState(1);
+  // Material-ui state for Dialog component rendering
+  const [open, setOpen] = useState(false);
+
+  // Proceed to next stage
+  const nextStage = () => {
+    setStage(stage + 1);
+  };
+
+  // Proceed to prev stage
+  const prevStage = () => {
+    setStage(stage - 1);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -31,20 +56,14 @@ const LogEmotionsButton = (props) => {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        {/* ----- This is the start of the Dialog Area ----- */}
+        <DialogTitle id="form-dialog-title">
+          Select a Primary Emotion you are feeling.
+        </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
+            anger, fear, sadness, disgust, surprise, anticipation, trust, or joy
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
@@ -54,6 +73,7 @@ const LogEmotionsButton = (props) => {
             Subscribe
           </Button>
         </DialogActions>
+        {/* ----- This is the end of the Dialog Area ----- */}
       </Dialog>
     </div>
   );

9089af67620673a081da2cbb3547d7d9982b3908
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-07 20:27:22 -0500

    added the LandingPage Component

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>HowDIF</h1>
+      <ul>
+        <li>
+          HowDIF is a tool that helps you understand yourself and helps identify
+          the emotions you are feeling. Your feelings and emotions are valid.
+          You deserve to understand them better.
+        </li>
+        <li>
+          HowDIF provides the process to dig deeper into your emotions, and the
+          data to analyze these emotions.
+        </li>
+        <li>
+          With this app you will explore your emotions deeper which will empower
+          you to understand your emotions.
+        </li>
+      </ul>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  );
+};
+
+export default LandingPage;

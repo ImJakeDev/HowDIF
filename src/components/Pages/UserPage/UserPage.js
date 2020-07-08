f609cc84bc6a61f0e4073dd01a1b4c35ca4c943f
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-08 09:38:11 -0500

    added UserPage.css and starting to work on UserPage functionality

--- src/components/Pages/UserPage/UserPage.js
+++ src/components/Pages/UserPage/UserPage.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../../Utilities/LogOutButton/LogOutButton';
+import "./UserPage.css";
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 const UserPage = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
-    <button>Log Emotion</button>
+  <div className="container">
+    <div>
+      <h1 id="welcome">Welcome, {props.user.username}!</h1>
+      <p>Your ID is: {props.user.id}</p>
+      <LogOutButton className="log-in" />
+      <button>Log Emotion</button>
+    </div>
   </div>
 );
 

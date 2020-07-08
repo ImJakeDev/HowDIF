ea7838b838c3703b0a2f2e75c83604eb5b74c65b
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-08 10:06:18 -0500

    added history back button on radar chart back to user home page

--- src/components/Charts/RadarChart/RadarChart.js
+++ src/components/Charts/RadarChart/RadarChart.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ResponsiveRadar } from "@nivo/radar";
+import { useHistory } from "react-router-dom";
 import "./RadarChart.css";
 
 // Went alright
@@ -40,68 +41,75 @@ function RadarChart() {
     },
   ]);
 
+  const history = useHistory();
+
   return (
     <>
-      <div className="boxSize">
-        <h1>Emotional Radar</h1>
-        <ResponsiveRadar
-          data={data}
-          keys={["user"]}
-          indexBy="emotion"
-          maxValue="auto"
-          margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-          curve="linearClosed"
-          borderWidth={2}
-          borderColor={{ from: "color" }}
-          gridLevels={5}
-          gridShape="circular"
-          gridLabelOffset={36}
-          enableDots={true}
-          dotSize={10}
-          dotColor={{ theme: "background" }}
-          dotBorderWidth={2}
-          dotBorderColor={{ from: "color" }}
-          enableDotLabel={true}
-          dotLabel="value"
-          dotLabelYOffset={-12}
-          colors={[
-            "#61cdbb",
-            "#32936F",
-            "#e8c1a0",
-            "#f47560",
-            "#F4E04D",
-            "#587792",
-            "#CBDFBD",
-            "#A53860",
-          ]}
-          fillOpacity={0.25}
-          blendMode="normal"
-          animate={true}
-          motionStiffness={90}
-          motionDamping={15}
-          isInteractive={true}
-          legends={[
-            {
-              anchor: "top-left",
-              direction: "column",
-              translateX: -50,
-              translateY: -40,
-              itemWidth: 80,
-              itemHeight: 20,
-              itemTextColor: "#999",
-              symbolSize: 12,
-              symbolShape: "circle",
-              effects: [
+      <div>
+        <center>
+          <h1>Emotional Radar</h1>
+          <button onClick={() => history.push("/home")}>Back</button>
+          <div className="boxSize">
+            <ResponsiveRadar
+              data={data}
+              keys={["user"]}
+              indexBy="emotion"
+              maxValue="auto"
+              margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+              curve="linearClosed"
+              borderWidth={2}
+              borderColor={{ from: "color" }}
+              gridLevels={5}
+              gridShape="circular"
+              gridLabelOffset={36}
+              enableDots={true}
+              dotSize={10}
+              dotColor={{ theme: "background" }}
+              dotBorderWidth={2}
+              dotBorderColor={{ from: "color" }}
+              enableDotLabel={true}
+              dotLabel="value"
+              dotLabelYOffset={-12}
+              colors={[
+                "#61cdbb",
+                "#32936F",
+                "#e8c1a0",
+                "#f47560",
+                "#F4E04D",
+                "#587792",
+                "#CBDFBD",
+                "#A53860",
+              ]}
+              fillOpacity={0.25}
+              blendMode="normal"
+              animate={true}
+              motionStiffness={90}
+              motionDamping={15}
+              isInteractive={true}
+              legends={[
                 {
-                  on: "hover",
-                  style: {
-                    itemTextColor: "#000",
-                  },
+                  anchor: "top-left",
+                  direction: "column",
+                  translateX: -50,
+                  translateY: -40,
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemTextColor: "#999",
+                  symbolSize: 12,
+                  symbolShape: "circle",
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemTextColor: "#000",
+                      },
+                    },
+                  ],
                 },
-              ],
-            },
-          ]}
-        />
+              ]}
+            />
+          </div>
+        </center>
       </div>
     </>
   );

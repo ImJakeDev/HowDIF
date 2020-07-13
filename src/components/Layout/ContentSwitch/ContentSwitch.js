14e6bc7ebbb0b73eab47acd1144bcd42cbca7417
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-12 23:53:33 -0500

    added SignInPage Component

--- src/components/Layout/ContentSwitch/ContentSwitch.js
+++ src/components/Layout/ContentSwitch/ContentSwitch.js
@@ -2,7 +2,7 @@
 import React, { Component } from "react";
 // React Router DOM
 import {
-  HashRouter as
+  HashRouter as Router,
   Route,
   Redirect,
   Switch,
@@ -17,6 +17,7 @@ import LandingPage from "../../Pages/LandingPage/LandingPage";
 import UserPage from "../../Pages/UserPage/UserPage";
 // import InfoPage from "../../Pages/InfoPage/InfoPage";
 import LoginPage from "../../Pages/LoginPage/LoginPage";
+import SignInPage from "../../Pages/SignInPage/SignInPage";
 import RegisterPage from "../../Pages/RegisterPage/RegisterPage";
 
 import PieChart from "../../Charts/PieChart/PieChart";
@@ -31,29 +32,32 @@ class Content extends Component {
 
   render() {
     return (
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
+      <Router>
+        <Switch>
+          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+          <Redirect exact from="/" to="/home" />
+          {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            {/* <Route exact path="/about" component={AboutPage} /> */}
-            <Route exact path="/landing" component={LandingPage} />
-            {/* <Route exact path="/login" component={LoginPage} />
+          {/* <Route exact path="/about" component={AboutPage} /> */}
+          <Route exact path="/landing" component={LandingPage} />
+          <Route exact path="/signin" component={SignInPage} />
+          {/* <Route exact path="/login" component={LoginPage} />
             <Route exact path="/register" component={RegisterPage} /> */}
-            {/* For protected routes, the view could show one of several things on the same route.
+          {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute exact path="/home" component={UserPage} />
-            {/* This works the same as the other protected route, except that if the user is logged in,
+          <ProtectedRoute exact path="/home" component={UserPage} />
+          {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            {/* <ProtectedRoute exact path="/info" component={InfoPage} /> */}
-            <ProtectedRoute exact path="/pie" component={PieChart} />
-            <ProtectedRoute exact path="/radar" component={RadarChart} />
-            <ProtectedRoute exact path="/calendar" component={CalendarChart} />
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
+          {/* <ProtectedRoute exact path="/info" component={InfoPage} /> */}
+          <ProtectedRoute exact path="/pie" component={PieChart} />
+          <ProtectedRoute exact path="/radar" component={RadarChart} />
+          <ProtectedRoute exact path="/calendar" component={CalendarChart} />
+          {/* If none of the other routes matched, we will show a 404. */}
+          <Route render={() => <h1>404</h1>} />
+        </Switch>
+      </Router>
     );
   }
 }

635360248205732b6213cfd78101b6f66d3ebbfe
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-15 11:29:56 -0500

    cleaned and commented code

--- src/components/Layout/Content/Content.js
+++ src/components/Layout/Content/Content.js
@@ -1,28 +1,21 @@
 // React
-import React, { Component } from "react";
-// React Router DOM
-import {
-  HashRouter as Router,
-} from "react-router-dom";
-// Components
+import React from "react";
+// React Router DOM imports
+import { HashRouter as Router } from "react-router-dom";
+// Component imports
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
-import ContentSwitch from "../ContentSwitch/ContentSwitch"
+import ContentSwitch from "../ContentSwitch/ContentSwitch";
 
-import "./Content.css";
-
-class Content extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Nav />
-          <ContentSwitch/>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-}
+// Content component is the start of the router and where Nav and Footer is at and all other content will be in between
+const Content = () => {
+  return (
+    <Router>
+      <Nav />
+      <ContentSwitch />
+      <Footer />
+    </Router>
+  );
+};
 
 export default Content;
